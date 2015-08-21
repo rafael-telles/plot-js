9e0837757c556daac88d93c259dad6270b689024
Rafael Telles <raftel.ti@gmail.com>
2015-08-21 20:10:20 -0300

    Refactoring, no more shared variables

--- dist/plot.js
+++ dist/plot.js
@@ -4897,27 +4897,83 @@ module.exports = vec4;
 
 },{"./common.js":2}],11:[function(require,module,exports){
 var glMatrix = window.glMatrix = require('gl-matrix');
-var matrix = window.matrix = glMatrix.mat2d.create();
-
-var invertedMatrix = glMatrix.mat2d.create();
-var topLeftCorner = glMatrix.vec2.create();
-var bottomRightCorner = glMatrix.vec2.create();
-
-var startPosition = glMatrix.vec2.create();
-var position = glMatrix.vec2.create();
-
-var lastDistance = 0;
 
 window.Plot = function Plot(selector, func) {
-	this.canvas = document.querySelector(selector);
-	this.context = this.canvas.getContext('2d');
+	var matrix = glMatrix.mat2d.create();
+	var invertedMatrix = glMatrix.mat2d.create();
+	var topLeftCorner = glMatrix.vec2.create();
+	var bottomRightCorner = glMatrix.vec2.create();
+
+	var startPosition = glMatrix.vec2.create();
+	var position = glMatrix.vec2.create();
+
+	var lastDistance = 0;
+
+	var canvas = this.canvas = document.querySelector(selector);
+	var context = this.canvas.getContext('2d');
+
+	var func = this.func = func;
 
 	glMatrix.mat2d.translate(matrix, matrix, [this.canvas.width * 0.5, this.canvas.height * 0.5]);
 	glMatrix.mat2d.scale(matrix, matrix, [50, -50]);
 
-	this.context.lineCap = 'round';
+	context.lineCap = 'round';
 
 	var self = this;
+
+	function clear() {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+	}
+	function updateMatrix() {
+		glMatrix.mat2d.invert(invertedMatrix, matrix);
+		glMatrix.vec2.transformMat2d(topLeftCorner, glMatrix.vec2.fromValues(0, 0), invertedMatrix);
+		glMatrix.vec2.transformMat2d(bottomRightCorner, glMatrix.vec2.fromValues(canvas.width, canvas.height), invertedMatrix);
+	}
+	function renderAxis() {
+		context.lineWidth = 1;
+		context.strokeStyle = 'gray';
+
+		context.beginPath();
+		context.moveTo(0, matrix[5]);
+		context.lineTo(canvas.width, matrix[5]);
+		context.stroke();
+
+		context.beginPath();
+		context.moveTo(matrix[4], 0);
+		context.lineTo(matrix[4], canvas.height);
+		context.stroke();
+
+		for(var x = matrix[4] % matrix[0]; x < canvas.width; x += matrix[0]) {
+			context.beginPath();
+			context.moveTo(x, matrix[5] - 5);
+			context.lineTo(x, matrix[5] + 5);
+			context.stroke();
+		}
+
+		for(var y = matrix[5] % matrix[0]; y < canvas.height; y += matrix[0]) {
+			context.beginPath();
+			context.moveTo(matrix[4] - 5, y);
+			context.lineTo(matrix[4] + 5, y);
+			context.stroke();
+		}
+	}
+	function render() {
+		clear();
+
+		updateMatrix();
+		renderAxis();	
+
+		context.lineWidth = 2;
+		context.strokeStyle = "black";
+		context.beginPath();
+		for(var x = topLeftCorner[0]; x < bottomRightCorner[0]; x += 1 / matrix[0]) {
+			var p = glMatrix.vec2.fromValues(x, func(x));
+			glMatrix.vec2.transformMat2d(p, p, matrix);
+			context.lineTo(p[0], p[1]);
+		}
+		context.stroke();
+	}
+
 	this.canvas.addEventListener('touchstart', function(e) {
 		position[0] = matrix[4];
 		position[1] = matrix[5];
@@ -4940,62 +4996,9 @@ window.Plot = function Plot(selector, func) {
 			lastDistance = distance;
 		}
 
-		self.render();
+		render();
 	});
 
-	this.func = func;
-
-	this.render();
-}
-Plot.prototype.clear = function() {
-	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-}
-Plot.prototype.updateMatrix = function() {
-	glMatrix.mat2d.invert(invertedMatrix, matrix);
-	glMatrix.vec2.transformMat2d(topLeftCorner, glMatrix.vec2.fromValues(0, 0), invertedMatrix);
-	glMatrix.vec2.transformMat2d(bottomRightCorner, glMatrix.vec2.fromValues(this.canvas.width, this.canvas.height), invertedMatrix);
-}
-Plot.prototype.renderAxis = function() {
-	this.context.lineWidth = 1;
-	this.context.strokeStyle = "gray";
-	this.context.beginPath();
-	this.context.moveTo(0, matrix[5]);
-	this.context.lineTo(this.canvas.width, matrix[5]);
-	this.context.stroke();
-
-	this.context.beginPath();
-	this.context.moveTo(matrix[4], 0);
-	this.context.lineTo(matrix[4], this.canvas.height);
-	this.context.stroke();
-
-	for(var x = matrix[4] % matrix[0]; x < this.canvas.width; x += matrix[0]) {
-		this.context.beginPath();
-		this.context.moveTo(x, matrix[5] - 5);
-		this.context.lineTo(x, matrix[5] + 5);
-		this.context.stroke();
-	}
-
-	for(var y = matrix[5] % matrix[0]; y < this.canvas.height; y += matrix[0]) {
-		this.context.beginPath();
-		this.context.moveTo(matrix[4] - 5, y);
-		this.context.lineTo(matrix[4] + 5, y);
-		this.context.stroke();
-	}
-};
-Plot.prototype.render = function() {
-	this.clear();
-
-	this.updateMatrix();
-	this.renderAxis();	
-
-	this.context.lineWidth = 2;
-	this.context.strokeStyle = "black";
-	this.context.beginPath();
-	for(var x = topLeftCorner[0]; x < bottomRightCorner[0]; x += 1 / matrix[0]) {
-		var p = glMatrix.vec2.fromValues(x, this.func(x));
-		glMatrix.vec2.transformMat2d(p, p, matrix);
-		this.context.lineTo(p[0], p[1]);
-	}
-	this.context.stroke();
+	render();
 };
 },{"gl-matrix":1}]},{},[11]);
