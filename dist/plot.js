5486e9f4166081013b52f53203b7822832134f42
Rafael Telles <raftel.ti@gmail.com>
2015-08-22 14:54:16 -0300

    Improving ticks, adding grid

--- dist/plot.js
+++ dist/plot.js
@@ -4930,6 +4930,65 @@ window.Plot = function Plot(selector, func) {
 		glMatrix.vec2.transformMat2d(bottomRightCorner, glMatrix.vec2.fromValues(canvas.width, canvas.height), invertedMatrix);
 	}
 	function renderAxis() {
+		context.font = "8px sans";
+		context.textBaseline = 'middle';
+		context.textAlign="center"; 
+
+		var coef = Math.pow(0.1, ~~(Math.log10(matrix[0])) - 1);
+		var step = coef * ([5,2,1][~~(3 * Math.log10(matrix[0])) % 3]);
+		console.log(step);
+
+		var firstX = topLeftCorner[0];
+		var lastX = bottomRightCorner[0];
+		firstX -= firstX % step;
+		lastX += lastX % step;
+
+
+		context.lineWidth = 0.5;
+		context.strokeStyle = 'lightgray';
+
+		var p;
+		for(var x = firstX; x <= lastX; x += step) {
+			if(x == 0) continue;
+			p = glMatrix.vec2.fromValues(x, 0);
+			glMatrix.vec2.transformMat2d(p, p, matrix);
+
+			context.beginPath();
+			context.moveTo(p[0], p[1] - 2);
+			context.lineTo(p[0], p[1] + 2);
+			context.stroke();
+
+ 			context.fillText(x.toFixed(4), p[0], p[1] + 8);
+
+ 			context.beginPath();
+			context.moveTo(p[0], 0);
+			context.lineTo(p[0], canvas.height);
+			context.stroke();
+		}
+		var firstY = bottomRightCorner[1];
+		var lastY = topLeftCorner[1];
+		firstY -= firstY % step;
+		lastY += lastY % step;
+
+		context.textAlign="right"; 
+		for(var y = firstY; y <= lastY; y += step) {
+			if(y == 0) continue;
+			p = glMatrix.vec2.fromValues(0, y);
+			glMatrix.vec2.transformMat2d(p, p, matrix);
+
+			context.beginPath();
+			context.moveTo(p[0] - 2, p[1]);
+			context.lineTo(p[0] + 2, p[1]);
+			context.stroke();
+
+ 			context.fillText(y.toFixed(4), p[0] - 4, p[1]);
+
+ 			context.beginPath();
+			context.moveTo(0, p[1]);
+			context.lineTo(canvas.width, p[1]);
+			context.stroke();
+		}
+
 		context.lineWidth = 1;
 		context.strokeStyle = 'gray';
 
@@ -4942,20 +5001,6 @@ window.Plot = function Plot(selector, func) {
 		context.moveTo(matrix[4], 0);
 		context.lineTo(matrix[4], canvas.height);
 		context.stroke();
-
-		for(var x = matrix[4] % matrix[0]; x < canvas.width; x += matrix[0]) {
-			context.beginPath();
-			context.moveTo(x, matrix[5] - 5);
-			context.lineTo(x, matrix[5] + 5);
-			context.stroke();
-		}
-
-		for(var y = matrix[5] % matrix[0]; y < canvas.height; y += matrix[0]) {
-			context.beginPath();
-			context.moveTo(matrix[4] - 5, y);
-			context.lineTo(matrix[4] + 5, y);
-			context.stroke();
-		}
 	}
 	function render() {
 		clear();
--- src/index - Copia.js
+++ src/index - Copia.js
@@ -1,4 +1,4 @@
-var glMatrix = window.glMatrix = require('gl-matrix');
+var glMatrix = require('gl-matrix');
 
 window.Plot = function Plot(selector, func) {
 	var matrix = glMatrix.mat2d.create();
@@ -32,64 +32,6 @@ window.Plot = function Plot(selector, func) {
 		glMatrix.vec2.transformMat2d(bottomRightCorner, glMatrix.vec2.fromValues(canvas.width, canvas.height), invertedMatrix);
 	}
 	function renderAxis() {
-		context.font = "8px sans";
-		context.textBaseline = 'middle';
-		context.textAlign="center"; 
-
-		var coef = Math.pow(0.1, ~~(Math.log10(matrix[0])) - 1);
-		var step = coef * ([5,2,1][~~(3 * Math.log10(matrix[0])) % 3]);
-
-		var firstX = topLeftCorner[0];
-		var lastX = bottomRightCorner[0];
-		firstX -= firstX % step;
-		lastX += lastX % step;
-
-
-		context.lineWidth = 0.5;
-		context.strokeStyle = 'lightgray';
-
-		var p;
-		for(var x = firstX; x <= lastX; x += step) {
-			if(x == 0) continue;
-			p = glMatrix.vec2.fromValues(x, 0);
-			glMatrix.vec2.transformMat2d(p, p, matrix);
-
-			context.beginPath();
-			context.moveTo(p[0], p[1] - 2);
-			context.lineTo(p[0], p[1] + 2);
-			context.stroke();
-
- 			context.fillText(x.toFixed(4), p[0], p[1] + 8);
-
- 			context.beginPath();
-			context.moveTo(p[0], 0);
-			context.lineTo(p[0], canvas.height);
-			context.stroke();
-		}
-		var firstY = bottomRightCorner[1];
-		var lastY = topLeftCorner[1];
-		firstY -= firstY % step;
-		lastY += lastY % step;
-
-		context.textAlign="right"; 
-		for(var y = firstY; y <= lastY; y += step) {
-			if(y == 0) continue;
-			p = glMatrix.vec2.fromValues(0, y);
-			glMatrix.vec2.transformMat2d(p, p, matrix);
-
-			context.beginPath();
-			context.moveTo(p[0] - 2, p[1]);
-			context.lineTo(p[0] + 2, p[1]);
-			context.stroke();
-
- 			context.fillText(y.toFixed(4), p[0] - 4, p[1]);
-
- 			context.beginPath();
-			context.moveTo(0, p[1]);
-			context.lineTo(canvas.width, p[1]);
-			context.stroke();
-		}
-
 		context.lineWidth = 1;
 		context.strokeStyle = 'gray';
 
@@ -102,6 +44,27 @@ window.Plot = function Plot(selector, func) {
 		context.moveTo(matrix[4], 0);
 		context.lineTo(matrix[4], canvas.height);
 		context.stroke();
+
+		context.font = "8px sans";
+		context.textBaseline = 'middle';
+		context.textAlign="center"; 
+		
+		var firstX = ~~topLeftCorner[0];
+		var lastX = ~~bottomRightCorner[0];
+		var stepX = (lastX - firstX) / 5;
+
+		var p;
+		for(var x = firstX; x <= lastX; x += stepX) {
+			p = glMatrix.vec2.fromValues(x, 0);
+			glMatrix.vec2.transformMat2d(p, p, matrix);
+
+			context.beginPath();
+			context.moveTo(p[0], p[1] - 5);
+			context.lineTo(p[0], p[1] + 5);
+			context.stroke();
+
+ 			context.fillText(x.toPrecision(5), p[0], p[1] + 12);
+		}
 	}
 	function render() {
 		clear();
@@ -112,8 +75,9 @@ window.Plot = function Plot(selector, func) {
 		context.lineWidth = 2;
 		context.strokeStyle = "black";
 		context.beginPath();
+		var p = glMatrix.vec2.create();
 		for(var x = topLeftCorner[0]; x < bottomRightCorner[0]; x += 1 / matrix[0]) {
-			var p = glMatrix.vec2.fromValues(x, func(x));
+			p = glMatrix.vec2.fromValues(x, func(x));
 			glMatrix.vec2.transformMat2d(p, p, matrix);
 			context.lineTo(p[0], p[1]);
 		}
@@ -131,15 +95,26 @@ window.Plot = function Plot(selector, func) {
 		}
 	});
 
-	this.canvas.addEventListener('touchmove', function(e) {
-		matrix[4] = position[0] + e.touches[0].clientX - startPosition[0];
-		matrix[5] = position[1] + e.touches[0].clientY - startPosition[1];
-		
+	this.canvas.addEventListener('touchmove', function(e) {		
 		if(e.touches.length === 2) {
 			var distance = Math.sqrt(Math.pow(e.touches[0].clientX - e.touches[1].clientX, 2) + Math.pow(e.touches[0].clientY - e.touches[1].clientY, 2));
+
+			var origin = glMatrix.vec2.create();
+			glMatrix.vec2.transformMat2d(origin, startPosition, invertedMatrix);
+
+			var p = glMatrix.vec2.create();
+			glMatrix.vec2.scale(p, origin, -1);
+
+			glMatrix.mat2d.translate(matrix, matrix, p);
 			glMatrix.mat2d.scale(matrix, matrix, [distance / lastDistance, distance / lastDistance]);
+			glMatrix.mat2d.translate(matrix, matrix, origin);
+
+			console.log(glMatrix.vec2.str(origin));
 
 			lastDistance = distance;
+		} else {
+			matrix[4] = position[0] + e.touches[0].clientX - startPosition[0];
+			matrix[5] = position[1] + e.touches[0].clientY - startPosition[1];
 		}
 
 		render();
