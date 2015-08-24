f1de27aeeb06e83e56c66f127e59a513ed972dc3
Rafael Telles <raftel.ti@gmail.com>
2015-08-24 11:19:14 -0300

    Inverting Y-axis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.Plot = function Plot(selector, func) {
 	var canvas = this.canvas = document.querySelector(selector);
-	var context = this.canvas.getContext('2d');
+	var context = canvas.getContext('2d');
+	var clientRect = canvas.getBoundingClientRect();
 
 	var func = this.func = func;
 
@@ -23,8 +24,8 @@ window.Plot = function Plot(selector, func) {
 		context.textBaseline = 'middle';
 		context.textAlign="center"; 
 
-		var coef = Math.pow(0.1, ~~(Math.log10(scale)) - 1);
-		var step = coef * ([5,2,1][~~(3 * Math.log10(scale)) % 3]) * scale;
+		var coef = Math.pow(0.1, ~~(Math.log(scale)/Math.log(10)) - 1);
+		var step = coef * ([5,2,1][~~(3 * Math.log(scale)/Math.log(10)) % 3]) * scale;
 
 		context.strokeStyle = 'lightgray';
 
@@ -55,7 +56,7 @@ window.Plot = function Plot(selector, func) {
 			context.lineTo(dx + 5, y);
 			context.stroke();
 			
- 			context.fillText(((y - dy)/scale).toFixed(4), dx - 4, y);
+ 			context.fillText(((dy - y)/scale).toFixed(4), dx - 4, y);
 
 			context.lineWidth = 0.5;
  			context.beginPath();
@@ -86,24 +87,42 @@ window.Plot = function Plot(selector, func) {
 		context.strokeStyle = "black";
 		context.beginPath();
 		for(var x = 0; x < canvas.width; x++) {
-			context.lineTo(x, scale * func((x - dx) / scale) + dy);
+			context.lineTo(x, -scale * func((x - dx) / scale) + dy);
 		}
 		context.stroke();
 	}
+
+	var startX, startY;
+	var mouseDown = false;
+	canvas.addEventListener('mousedown', function(e) {
+		mouseDown = true;
+		startX = dx - e.clientX;
+		startY = dy - e.clientY;
+	});
+	canvas.addEventListener('mouseup', function(e) {
+		mouseDown = false;
+	})
+	canvas.addEventListener('mousemove', function(e) {
+		if(mouseDown) {
+			dx = e.clientX + startX;
+			dy = e.clientY + startY;
+
+			render();
+		}
+	});
 	
-	var startTouchX, startTouchY;
-	this.canvas.addEventListener('touchstart', function(e) {
-		startTouchX = dx - e.touches[0].clientX;
-		startTouchY = dy - e.touches[0].clientY;
+	canvas.addEventListener('touchstart', function(e) {
+		startX = dx - e.touches[0].clientX;
+		startY = dy - e.touches[0].clientY;
 
 		if(e.touches.length === 2) {
 			lastDistance = Math.sqrt(Math.pow(e.touches[0].clientX - e.touches[1].clientX, 2) + Math.pow(e.touches[0].clientY - e.touches[1].clientY, 2));
 		}
 	});
 	
-	this.canvas.addEventListener('touchmove', function(e) {
-		dx = e.touches[0].clientX + startTouchX;
-		dy = e.touches[0].clientY + startTouchY;
+	canvas.addEventListener('touchmove', function(e) {
+		dx = e.touches[0].clientX + startX;
+		dy = e.touches[0].clientY + startY;
 		
 		if(e.touches.length === 2) {
 			var distance = Math.sqrt(Math.pow(e.touches[0].clientX - e.touches[1].clientX, 2) + Math.pow(e.touches[0].clientY - e.touches[1].clientY, 2));
@@ -117,13 +136,12 @@ window.Plot = function Plot(selector, func) {
 		render();
 	});
 
-	this.canvas.addEventListener('mousewheel', function(e) {
+	canvas.addEventListener('wheel', function(e) {
 		var multiplier = 0.9;
-		if(e.wheelDelta > 0) {
+		if(e.deltaY < 0) {
 			multiplier = 1.1;
 		}
-
-		zoom(multiplier, e.x, e.y);
+		zoom(multiplier, e.clientX - clientRect.left, e.clientY - clientRect.top);
 		render();
 	});
 	
